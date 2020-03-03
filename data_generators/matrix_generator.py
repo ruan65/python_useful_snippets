812939a81507b70e795c6a7447c99d0a6b47b2c3
ruan65 <ruan65@gmail.com>
2020-03-04 02:13:24 +0300

    save

--- data_generators/matrix_generator.py
+++ data_generators/matrix_generator.py
@@ -13,3 +13,15 @@ def rand_matrix(rows, columns, minmax):
 matrix = rand_matrix(5, 5, 5)
 
 df = pd.DataFrame(matrix)
+
+df2 = pd.concat([df, df])
+
+df2.reset_index()
+
+df4 = pd.concat([df2, df2], axis=1)
+
+df4.reset_index(inplace=True)
+
+df4.columns = [i for i in range(len(df4.columns))]
+
+df4.rolling(5, min_periods=2).mean()
